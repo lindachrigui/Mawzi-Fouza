5f328bf6e98fb2226402cf3975ee6c1f682cdfae
Ayyoub <ayyoub.mkadmi3@gmail.com>
2024-11-08 00:00:05 +0100

    Classes

---
+++ src/components/Classes.tsx
@@ -0,0 +1,15 @@
+import ImageCarousel from "./ImageCarousel";
+import "./Classes.css";
+
+function Classes() {
+  return (
+    <>
+      <div className="Classes">
+        <h1>Classes</h1>
+        <ImageCarousel />
+      </div>
+    </>
+  );
+}
+
+export default Classes;
